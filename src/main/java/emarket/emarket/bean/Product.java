d26694b0dd782a97518a8c7c4aab16a4fed7493e
mohankarki54 <mohankarki54@gmail.com>
2020-07-11 14:07:19 -0500

    Added 3 bean for OrderDetail, Product, ProductRating.

---
+++ src/main/java/emarket/emarket/bean/Product.java
@@ -0,0 +1,71 @@
+package emarket.emarket.bean;
+
+public class Product {
+
+    int productid;
+    String name;
+    String description;
+    Float price;
+    String productType;
+    String type;
+    int sellerid;
+
+    //For image define
+
+    public int getSellerid() {
+        return sellerid;
+    }
+
+    public void setSellerid(int sellerid) {
+        this.sellerid = sellerid;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getProductType() {
+        return productType;
+    }
+
+    public void setProductType(String productType) {
+        this.productType = productType;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getProductid() {
+        return productid;
+    }
+
+    public void setProductid(int productid) {
+        this.productid = productid;
+    }
+
+}
