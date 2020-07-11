32fb1fc5f13c24ca2ca39d8ab364585356494eab
mohankarki54 <mohankarki54@gmail.com>
2020-07-11 13:14:44 -0500

    Password encryption using Bcrypt, login and registration update through unique email address.

--- src/main/java/emarket/emarket/controller/HomeController.java
+++ src/main/java/emarket/emarket/controller/HomeController.java
@@ -1,15 +1,22 @@
 package emarket.emarket.controller;
 
+import emarket.emarket.bean.Account;
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import emarket.emarket.bean.User;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
     @GetMapping("/home")
     public  String check(){
@@ -17,29 +24,77 @@ public class HomeController {
     }
 
     @GetMapping(path = "/login")
-    public String login(){
-        //Prints from the database displaying the login bar
-
+    public String login(Model model){
+        Account account = new Account();
+        model.addAttribute("account", account);
         return "login";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String dologin(@ModelAttribute("account") Account account){
+        System.out.println(account.getEmail());
+        System.out.println(account.getPassword());
+        String sql = "SELECT pass FROM user WHERE email = ?";
+        String userpass = jdbcTemplate.queryForObject(sql, new Object[]{account.getEmail()}, String.class);
+        System.out.println(userpass);
+        boolean status = checkPass(account.getPassword(),userpass);
+        if (status){
+            return "home";
+        }
+        else{
+            return "login";
+        }
     }
 
     @GetMapping(path = "/signup")
     public String singup(Model model){
-        User user = new User();
-        model.addAttribute("user", user);
+        User user1 = new User();
+        model.addAttribute("user1", user1);
         return "signup";
     }
 
     @RequestMapping(value = "/signup", method = RequestMethod.POST)
-    public String doRegistration(@ModelAttribute("user") User user){
-        System.out.println(user.getFirstname());
-        System.out.println(user.getLastname());
-        System.out.println(user.getEmail());
-        return "home";
+    public String doRegistration(@ModelAttribute("user1") User user){
+
+        if(isUniqueEmail(user.getEmail())){
+            jdbcTemplate.update(
+                    "INSERT INTO  user(firstname, lastname, email, pass) VALUES (?, ?, ?, ?)", user.getFirstname(), user.getLastname(), user.getEmail(), BCrypt.hashpw(user.getPassword(), BCrypt.gensalt()));
+            return "home";
+        }
+        else {
+            System.out.println("Error email address already exits");
+            return "signup";
+        }
+
+
     }
 
-
     @GetMapping(path = "/about")
-    public String aboutus(){ return "about"; }
+    public String aboutus(){
+        return "about";
+    }
+
+    private boolean checkPass(String plainPassword, String hashedPassword) {
+        if (BCrypt.checkpw(plainPassword, hashedPassword))
+            System.out.println("The password matches.");
+        else {
+            System.out.println("The password does not match.");
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isUniqueEmail(String email){
+        boolean status = true;
+        List<String> allemail = new ArrayList<>();
+        allemail.addAll(jdbcTemplate.queryForList("select email from user;", String.class));
+        for(int i = 0; i < allemail.size(); i++){
+            if (email.equals(allemail.get(i))){
+                status =  false;
+                break;
+            }
+        }
+        return status;
+    }
 
 }
